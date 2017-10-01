636bce8e1b729ff2bf42f2d2749880be433b4b56
kPatch <irvsteve@gmail.com>
2017-10-01 18:35:45 -0400

    Read smartdesk state data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'user strict';
 var config      = require('./config.js');
 var firebase    = require('firebase');
-// var rpio        = require('rpio');
+var rpio        = require('rpio');
 var SerialPort = require('serialport');
 
 firebase.initializeApp(config.firebase.default);
@@ -10,15 +10,26 @@ firebase.initializeApp(config.firebase.default);
 // GPIO 15 RX   // PIN 10 Read Serial Message
 // GPIO 18 Read // PIN 12 Read Status by polling - There are not Interrupts in Raspberry PI
 // rpio.open(8, rpio.OUTPUT, rpio.LOW);
+// var txBuff = Buffer.alloc(10);
+const pirPin = 12;
+var pirBuffer = Buffer.alloc(10);
+rpio.open(pirPin, rpio.INPUT);   // Read-Only Input
+rpio.poll(pirPin, function() {
+    console.log('TEST:::');
+});
 
+// Open port
 var port = new SerialPort('/dev/ttyS0', {
     baudRate: 9600
 });
-
 // Set event handler for errors
 port.on('error', function(err) {
   console.log('LOG Error: ', err.message);
-})
+});
+// Read data
+port.on('readable', function () {
+  console.log('Data:', port.read());
+});
 
 // SmartDesk Protocols
 // Reference: https://nodejs.org/api/buffer.html
@@ -29,8 +40,9 @@ const button2Buffer             = Buffer.from([216, 216, 102, 8, 8]);
 const button3Buffer             = Buffer.from([216, 216, 102, 16, 16]);
 const button4Buffer             = Buffer.from([216, 216, 102, 32, 32]);
 const buttonConfigureBuffer     = Buffer.from([216, 216, 102, 64, 64]);
-
+    
 var smartdeskStateRef = firebase.database().ref('smartdeskState');
+
 smartdeskStateRef.on('value', function(snapshot) {
     console.log(snapshot.val());
 
