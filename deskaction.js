426416bd08d7348e0b12f385adee2e680d2e21e6
kPatch <irvsteve@gmail.com>
2017-10-04 03:40:40 -0400

    Create module that specifies desk actions

---
+++ deskaction.js
@@ -0,0 +1,16 @@
+var deskAction = {};
+
+deskAction.status = {};
+deskAction.status.EXECUTE    = 'EXECUTE';
+deskAction.status.EXECUTING  = 'EXECUTING';
+deskAction.status.COMPLETED  = 'COMPLETED';
+
+deskAction.command = {};
+deskAction.command.LOWER        = 'LOWER';
+deskAction.command.RAISE        = 'RAISE';
+
+deskAction.type = {};
+deskAction.type.NUMERIC  = 'QUANTITATIVE';
+deskAction.type.DISCRETE = 'QUALITATIVE';
+
+module.exports = deskAction;
