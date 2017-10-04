716c56dbc95304e8d6bd8ff789d6ed025b300d04
kPatch <irvsteve@gmail.com>
2017-10-03 23:35:39 -0400

    Add module with desk trigger command protocol

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 'user strict';
 const config      = require('./config.js');
+const desktrigger = require('./desktrigger');
 const firebase    = require('firebase');
-const rpio        = require('rpio');
 const SerialPort  = require('serialport');
+// const rpio        = require('rpio');
+
+/* Get terminal arguments 
+ * --env    :   'dev', 'prod'
+ * --action :   'up', 'down' // Only used when --env = dev
+ * --val    :   {numeric value, discrete value}
+ */
+// var grab = function(flag) {
+//     var index = process.argv.indexOf(flag);
+//     return (index === -1) ? null : process.argv[index+1];
+// }
+const grab = (flag) => {
+    var index = process.argv.indexOf(flag);
+    return (index === -1) ? null : process.argv[index+1];
+}
+const env = grab('--env');    // Environment: 'dev' 'prod'
+
+// var hexToDec = function(val) {
+//     return parseInt(val, 16);//.toString(10);
+// }
+const hexToDec = val => { return parseInt(val, 16); }
 
 /* GPIO Configuration */
 // GPIO 14 TX   // PIN 08 Transmit Serial Message
@@ -19,7 +40,7 @@ var pirBuffer = Buffer.alloc(10);
 const ByteLength = SerialPort.parsers.ByteLength;
 const port = new SerialPort('/dev/ttyS0', { baudRate: 57600 });
 const parser = port.pipe(new ByteLength({length: 8}));
-
+// SerialPort event handlers
 port.open(function(err) {
     if(err) {
         return console.log('Error opening port: ', err.message);
@@ -27,17 +48,10 @@ port.open(function(err) {
         console.log('Serial port opened');
     }
 });
-
 port.on('open', function() { console.log('JUST OPENED PORT'); });
-
-// Event handler for errors
 port.on('error', function(err) {
     console.log('LOG Error: ', err.message);
 });
-
-var hexToDec = function(val) {
-    return parseInt(val, 16);//.toString(10);
-}
 
 var deskState = [];
 deskState[0] = 0;
@@ -64,7 +78,10 @@ port.on('data', function(data) {
             pointer ++;
         } else {
             if (pointer == 13) {
-                console.log(`< ${hexBuf[0]}, ${hexBuf[1]}, ${hexBuf[2]}, ${hexBuf[3]}, ${hexBuf[4]}, ${hexBuf[5]}, ${hexBuf[6]}, ${hexBuf[7]}, ${hexBuf[8]}, ${hexBuf[9]}, ${hexBuf[10]}, ${hexBuf[11]}, ${hexBuf[12]} >`);
+                console.log(`< ${hexBuf[0]}, ${hexBuf[1]}, ${hexBuf[2]}, ${hexBuf[3]}\
+                    , ${hexBuf[4]}, ${hexBuf[5]}, ${hexBuf[6]}, ${hexBuf[7]}\
+                    , ${hexBuf[8]}, ${hexBuf[9]}, ${hexBuf[10]}, ${hexBuf[11]}, ${hexBuf[12]} >`);
+
                 deskState[0] = hexBuf[11];
                 deskState[1] = hexBuf[12]
                 pointer = 0;
@@ -91,33 +108,32 @@ port.on('data', function(data) {
     */
 });
 
-/* SmartDesk Communication Protocol */
-// Reference: https://nodejs.org/api/buffer.html
-const buttonIdleBuffer          = Buffer.from([216, 216, 102, 0, 0]);
-const buttonDownBuffer          = Buffer.from([216, 216, 102, 1, 1]);
-const buttonUpBuffer            = Buffer.from([216, 216, 102, 2, 2]);
-const button1Buffer             = Buffer.from([216, 216, 102, 4, 4]);
-const button2Buffer             = Buffer.from([216, 216, 102, 8, 8]);
-const button3Buffer             = Buffer.from([216, 216, 102, 16, 16]);
-const button4Buffer             = Buffer.from([216, 216, 102, 32, 32]);
-const buttonConfigureBuffer     = Buffer.from([216, 216, 102, 64, 64]);
-const hex                       = Buffer.from([0xD8, 0xD8, 0x66, 0x01, 0x01]);
-
 /* Firebase Configuration */
 firebase.initializeApp(config.firebase.default);
 const smartdeskStateRef = firebase.database().ref('smartdeskState');
 smartdeskStateRef.on('value', function(snapshot) {
     console.log(snapshot.val());
-    targetStateTop = 85;
-    targetStateBottom = 75;
+    targetStateTop      = 85;
+    targetStateBottom   = 75;
+
+    var targetHeight = (grab('action')!=null && grab('val') != null) ? grab('val') : -1;
     for(var i = 0; i < 15; i++) {
-        if(deskState[0] < targetStateTop) {
-            port.write(buttonUpBuffer, function(err, results) {
+        if(deskState[0] < targetHeight) {
+            port.write(desktrigger.up, function(err, results) {
                 if (err) {
                     return console.log('Error on write: ', err.message);
                 }
                 deskStatus = 'EXECUTING';
+                console.log('RAISING DESK');
             });
+        } else {
+            port.write(desktrigger.down, function(err, results) {
+                if (err) {
+                    return console.log('Error on write: ', err.message);
+                }
+                deskStatus = 'EXECUTING';
+                console.log('DECREASING DESK');
+            });            
         }
     }
 });
