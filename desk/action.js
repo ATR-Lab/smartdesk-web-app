594e023eb3bd79f9770f68d8b869e9a87ae9891f
Irvin Steve Cardenas <irvsteve@gmail.com>
2017-10-05 18:54:21 -0400

    Add "additional desk action value definitions

--- desk/action.js
+++ desk/action.js
@@ -4,11 +4,18 @@ deskAction.status = {};
 deskAction.status.EXECUTE    = 'EXECUTE';
 deskAction.status.EXECUTING  = 'EXECUTING';
 deskAction.status.COMPLETED  = 'COMPLETED';
+deskAction.status.RESET      = 'RESET';
 
 deskAction.command = {};
 deskAction.command.LOWER     = 'LOWER';
 deskAction.command.RAISE     = 'RAISE';
 deskAction.command.IDLE      = 'IDLE';
+deskAction.command.value        = {};
+deskAction.command.value.SMALL  = 'SMALL';
+deskAction.command.value.LARGE  = 'LARGE';
+deskAction.command.value.TOP    = 'TOP';
+deskAction.command.value.MIDDLE = 'MIDDLE';
+deskAction.command.value.BOTTOM = 'BOTTOM';
 
 deskAction.type = {};
 deskAction.type.NUMERIC      = 'QUANTITATIVE';
